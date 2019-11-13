adcaec7316db165d90f2796fc836f1936f47cda9
z1183186689 <1183186689@qq.com>
2019-11-13 17:04:01 +0800

    瞅瞅

--- demo/test_element.py
+++ demo/test_element.py
@@ -2,6 +2,7 @@ from time import sleep
 
 import autoit
 from selenium.webdriver import ActionChains#导入类
+from selenium.webdriver.common.keys import Keys
 
 
 def test_input(driver):#纯输入框，输入内容
@@ -95,3 +96,44 @@ def test_file2(driver):#上传文件，图片，等等
     sleep(2)
     autoit.control_click("打开", "Button1")
     sleep(2)
+
+def test_alert(driver):
+    driver.get("http://192.168.1.128:8082/xuepl/demo.html")
+    sleep(2)
+
+
+    button = driver.find_element_by_xpath("/html/body/table/tbody/tr[6]/td[2]/input")
+    button.click()
+    sleep(2)
+    alert=driver.switch_to.alert
+    alert.send_keys("sadasf")
+    alert.accept()
+    sleep(2)
+
+
+
+def test_windows(driver):
+    driver.get("http://192.168.1.128:8082/xuepl/demo.html")
+    sleep(2)
+
+    dang_dang = driver.find_element_by_link_text("当当")
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).click(dang_dang).key_up(Keys.CONTROL).perform()
+    sleep(2)
+    jd = driver.find_element_by_link_text("京东")
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).click(jd).key_up(Keys.CONTROL).perform()
+    sleep(2)
+    dn = driver.find_element_by_partial_link_text("度娘")
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).click(dn).key_up(Keys.CONTROL).perform()
+    sleep(2)
+
+    # 获取所有窗口的句柄
+    handles = driver.window_handles
+    for h in handles:
+        # 根据窗口句柄，切换窗口
+        driver.switch_to.window(h)
+        sleep(2)
+        if driver.title.__contains__("京东"):
+            break
